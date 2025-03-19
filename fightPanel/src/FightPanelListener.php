6c044fb32214e71bd71282e5f12566ddcec5f3a9
Sunch <120295462+Sunch233@users.noreply.github.com>
2025-03-19 21:41:08 +0800

    enchant

--- fightPanel/src/FightPanelListener.php
+++ fightPanel/src/FightPanelListener.php
@@ -7,6 +7,8 @@ namespace fightPanel;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\player\PlayerMissSwingEvent;
+use pocketmine\player\Player;
+use pocketmine\Server;
 
 class FightPanelListener implements Listener{
 
@@ -18,7 +20,7 @@ class FightPanelListener implements Listener{
 		if(!array_key_exists($playerName, $this->plugin->attackCount)){
 			$this->plugin->attackCount[$playerName] = array();
 		}
-		array_push($this->plugin->attackCount[$playerName], $this->plugin->get_total_millisecond());
+		$this->plugin->attackCount[$playerName][] = $this->plugin->get_total_millisecond();
 	}
 
 	public function onEntityDamageByEntity(EntityDamageByEntityEvent $event):void{
@@ -36,34 +38,30 @@ class FightPanelListener implements Listener{
 		if(!array_key_exists($playerName, $this->plugin->attackCount)){
 			$this->plugin->attackCount[$playerName] = array();
 		}
-		array_push($this->plugin->attackCount[$playerName], $this->plugin->get_total_millisecond());
+		$this->plugin->attackCount[$playerName][] = $this->plugin->get_total_millisecond();
 
 		// 记录输出伤害量
 		$playerName = $event->getDamager()->getName();
-		if($this->plugin->fromNameGetPlayer($playerName) == null){
+		if(Server::getInstance()->getPlayerExact($playerName) == null){
 			return;
 		}
 		if(!array_key_exists($playerName, $this->plugin->outDamageCount)){
 			$this->plugin->outDamageCount[$playerName] = array();
 		}
-		array_push($this->plugin->outDamageCount[$playerName], 
-			[
-				$this->plugin->get_total_millisecond(), $event->getFinalDamage()
-			]
-		);
+		$this->plugin->outDamageCount[$playerName][] = [
+			$this->plugin->get_total_millisecond(), $event->getFinalDamage()
+		];
 		
 		// 记录输入伤害量
 		$playerName = $event->getEntity()->getName();
-		if($this->plugin->fromNameGetPlayer($playerName) == null){
+		if(Server::getInstance()->getPlayerExact($playerName) == null){
 			return;
 		}
 		if(!array_key_exists($playerName, $this->plugin->inDamageCount)){
 			$this->plugin->inDamageCount[$playerName] = array();
 		}
-		array_push($this->plugin->inDamageCount[$playerName], 
-			[
-				$this->plugin->get_total_millisecond(), $event->getFinalDamage()
-			]
-		);
+		$this->plugin->inDamageCount[$playerName][] = [
+			$this->plugin->get_total_millisecond(), $event->getFinalDamage()
+		];
 	}
 }
